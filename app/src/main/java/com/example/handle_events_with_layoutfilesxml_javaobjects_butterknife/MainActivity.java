743f4086a703f2a932f31f580570623530ba1492
Aman Sharma <amansharma.11@hotmail.com>
2019-07-07 15:19:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/handle_events_with_layoutfilesxml_javaobjects_butterknife/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.handle_events_with_layoutfilesxml_javaobjects_butterknife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
